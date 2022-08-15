af9d7f968832eea2487378d7f62f8af16a9fba8a
m249865 <morales.chris@heb.com>
2022-08-15 15:23:06 -0500

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pat-rv-interview-angular';
+  title = 'pat-rv-angular-nterview';
 }
